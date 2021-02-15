39db3ac116f55c4a3a844cfb69332ea54f16c9e5
Erich Smith <erich.smith@jupiterone.com>
2021-02-15 17:37:43 -0500

    Memoize token list

--- src/npmTokens.js
+++ src/npmTokens.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const sh = require('child_process').execSync;
+let tokenList;
 
 function fetchNPMTokens() {
-  return JSON.parse(sh('npm token list --json').toString());
+  if (!tokenList) {
+    tokenList = sh('npm token list --json').toString();
+  }
+  return JSON.parse(tokenList);
 }
 
 module.exports = fetchNPMTokens;

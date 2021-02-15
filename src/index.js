cdedc4acbe00e9a322533ce6694247fd618a5f0a
Erich Smith <erich.smith@jupiterone.com>
2021-02-15 18:05:58 -0500

    Implement NPM token readonly policy enforcement

--- src/index.js
+++ src/index.js
@@ -45,4 +45,48 @@ function enforceMaxNPMTokenAge(days) {
   return process.exitCode === 0;
 }
 
-module.exports = enforceMaxNPMTokenAge;
+function enforceNPMTokenReadOnly() {
+  const illegalTokens = [];
+
+  process.exitCode = 0;
+
+  let tokens = [];
+  try {
+    // Prevent Yarn from overriding registry
+    // See https://github.com/yarnpkg/yarn/issues/2935
+    const original_config_registry = process.env['npm_config_registry'];
+    delete process.env['npm_config_registry'];
+
+    tokens = fetchNPMTokens();
+
+    process.env['npm_config_registry'] = original_config_registry;
+  } catch (err) {
+    console.error(err);
+    console.log('Please login with "npm login".');
+    process.exitCode = 1;
+    return false;
+  }
+
+  tokens.forEach(token => {
+    const publishToken = token.readonly === false;
+    const automationToken = token.automation === true;
+    if (publishToken && !automationToken) {
+      illegalTokens.push(token);
+    }
+  });
+
+  illegalTokens.forEach(token => {
+    const id = token.key.substr(0, 6);
+    console.log('Your npm token with id ' + id + ' is set to read/write! This is against policy.');
+    console.log('It was created on ' + token.created);
+    console.log('Please take the following steps:');
+    console.log('1. npm token create --read-only');
+    console.log('2. Paste this new token into your ~/.npmrc');
+    console.log('3. npm token revoke ' + id);
+    console.log();
+    process.exitCode = 1;
+  });
+  return process.exitCode === 0;
+}
+
+module.exports = { enforceMaxNPMTokenAge, enforceNPMTokenReadOnly };
